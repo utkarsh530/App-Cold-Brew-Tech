1f18f967d2b7f730809b29f38beade861f9d7beb
Utkarsh <37050056+utkarsh530@users.noreply.github.com>
2018-11-13 18:07:27 +0530

    Optimised Code

--- app/src/main/java/com/example/utkarsh/appcoldbrewtech/ViewModel/ViewModel.kt
+++ app/src/main/java/com/example/utkarsh/appcoldbrewtech/ViewModel/ViewModel.kt
@@ -4,7 +4,6 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.graphics.BitmapFactory
-import android.util.Log
 import com.example.utkarsh.appcoldbrewtech.Model.Image
 import okhttp3.*
 import org.json.JSONObject
@@ -41,15 +40,14 @@ class ViewModel : ViewModel() {
                     val jsonArray = jsonObject.getJSONArray("data")
                     val iObject: Array<JSONObject?> = arrayOfNulls(4)
                     val images: Array<Image?> = arrayOfNulls(4)
-                    val message: Array<String?> = arrayOfNulls(4)
                     val imageurl: Array<String?> = arrayOfNulls(4)
                     for (i in 0..3) {
                         iObject[i] = jsonArray.getJSONObject(i)
-                        Log.e("Hello", iObject[i]!!.getString("text"))
-                        message[i] = iObject[i]!!.getString("text")
                         imageurl[i] = iObject[i]?.getString("image_url")
-                        images[i] = Image(BitmapFactory.decodeStream(URL(imageurl[i]).openConnection().getInputStream()),iObject[i]!!.getString("text"))
-                        Log.e("Hello", "${images[i]?.message} ${message[i]}")
+                        images[i] = Image(
+                            BitmapFactory.decodeStream(URL(imageurl[i]).openConnection().getInputStream()),
+                            iObject[i]!!.getString("text")
+                        )
                     }
                     data.postValue(images)
                 }
