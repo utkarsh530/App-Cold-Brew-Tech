8b7390ddf3f4268aa3daa4d6e5fbdba7d244956e
Utkarsh <37050056+utkarsh530@users.noreply.github.com>
2018-11-13 14:15:20 +0530

    Added View Layout

--- app/src/main/java/com/example/utkarsh/appcoldbrewtech/View/MainActivity.kt
+++ app/src/main/java/com/example/utkarsh/appcoldbrewtech/View/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.utkarsh.appcoldbrewtech
+package com.example.utkarsh.appcoldbrewtech.View
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.utkarsh.appcoldbrewtech.R
 
 class MainActivity : AppCompatActivity() {
 
